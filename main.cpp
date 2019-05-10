8a3840ddc96e7711413fb23ea667af518d991fba
Victor Chimenti <chimenti@seattleu.edu>
2019-05-09 19:10:12 -0700

    added hw5 instructions

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+/***
+ *
+ * 5. Create a very simple program (C, C++, Java, Python) that contains a Loop, If, switch, and then create a Control
+ * Flow Diagram from it. The program should be less than 20 lines. Try to achieve a Cyclomatic Complexity of at least
+ * 2. Please use a tool (like Visio/Paint) to create the Control Flow Diagram. We have talked about this in Thursday's
+ * class, and plenty of examples on Control Flow Diagrams on web.
+ *
+ */
+
+
+
 #include <iostream>
 
 int main() {
